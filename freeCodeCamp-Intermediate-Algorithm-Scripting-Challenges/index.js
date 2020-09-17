d403e9e45fc500aea03ffa42c365f68b0b7f73c5
Miguel Manalo <miguelmanalo@protonmail.com>
2020-09-17 11:16:21 -0400

    Update index.js
    sorted union blank slate

--- freeCodeCamp-Intermediate-Algorithm-Scripting-Challenges/index.js
+++ freeCodeCamp-Intermediate-Algorithm-Scripting-Challenges/index.js
@@ -401,10 +401,31 @@ const fearNotLetter = str => {
     return undefined;
 }
   
-console.log(fearNotLetter("abce")); // 'd'
-console.log(fearNotLetter("abcdefghjklmno"));   // 'i'
-console.log(fearNotLetter("stvwx"));    // 'u'
-console.log(fearNotLetter("bcdf")); // 'e'
-console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));   // undefined
+// console.log(fearNotLetter("abce")); // 'd'
+// console.log(fearNotLetter("abcdefghjklmno"));   // 'i'
+// console.log(fearNotLetter("stvwx"));    // 'u'
+// console.log(fearNotLetter("bcdf")); // 'e'
+// console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));   // undefined 
+
+///////////////////////////////////////////////////////////////////////////////
+// Sorted Union
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sorted-union
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+// Check the assertion tests for examples.
+
+const uniteUnique = arr => {
+    
+    return arr;
+}
+  
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));  // [1, 3, 2, 5, 4]
+console.log(uniteUnique([1, 2, 3], [5, 2, 1])); // [1, 2, 3, 5]
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])); // [1, 2, 3, 5, 4, 6, 7, 8]
+
 ///////////////////////////////////////////////////////////////////////////////
 // 
