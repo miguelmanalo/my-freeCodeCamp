08d4fedd82246a09eb80d961c1ae211253c3b323
Miguel Manalo <miguelmanalo@protonmail.com>
2020-10-06 10:12:25 -0400

    Update index.js
    finished orbital period calculator

--- JavaScript Algorithms and Data Structures/9 - Intermediate-Algorithm-Scripting-Challenges/index.js
+++ JavaScript Algorithms and Data Structures/9 - Intermediate-Algorithm-Scripting-Challenges/index.js
@@ -924,4 +924,40 @@ const Person = function (firstAndLast) {
 // console.log(bob.getLastName()); // should return "Curry" after bob.setFullName("Haskell Curry")
 
 /// //////////////////////////////////////////////////////////////////////
-// 
+// Map the Debris
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/map-the-debris
+// Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+
+// The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+// You can read about orbital periods on Wikipedia.
+
+// The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+// The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+
+function orbitalPeriod(arr) {
+  // make a copy of the argument
+  const argCopy = arr.slice();
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  // iterate thru the array arg and assign each structure it's orbital period
+  for (let i = 0, len = argCopy.length; i < len; i += 1) {
+    const averageAlt = argCopy[i].avgAlt;
+    const orbitalPeriodCalc = Math.round(
+      2 * Math.PI * Math.sqrt((averageAlt + earthRadius) ** 3 / GM)
+    );
+    delete argCopy[i].avgAlt;
+    argCopy[i].orbitalPeriod = orbitalPeriodCalc;
+  }
+  return argCopy;
+}
+
+// console.log(orbitalPeriod([{ name: 'sputnik', avgAlt: 35873.5553 }])); // should return [{name: "sputnik", orbitalPeriod: 86400}]
+// console.log(
+//   orbitalPeriod([
+//     { name: 'iss', avgAlt: 413.6 },
+//     { name: 'hubble', avgAlt: 556.7 },
+//     { name: 'moon', avgAlt: 378632.553 },
+//   ])
+// ); // should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}]
